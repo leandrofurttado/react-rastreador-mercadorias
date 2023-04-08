074e946f1806fe1a9c0e64cf0a2d0ea6bc9f0e20
Leandro Sousa <leandro.souza@southti.com.br>
2023-04-08 19:58:01 -0300

    Primeiro commit do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { RiSearchEyeLine, RiShoppingCartFill } from "react-icons/ri";
+import './tracker.css';
+// o projeto é focado em realizar a busca por codigo de barras do correios
 
 function App() {
+  const [input, setInput] = useState('');
+  console.log(input)
+
+  function handleSearch(){
+    alert("Mercadoria encontrada!")
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">BLOR Tracker Mercadorias <RiShoppingCartFill size={50} /></h1>
+
+      <div className="containerInput">
+        <input 
+        type="text"
+        placeholder="Digite o código de rastreio..."
+        value={input}
+        onChange={
+          (e)=>setInput(e.target.value)
+        }
+        />
+
+        <button className="buttonInput" onClick={handleSearch}>
+          <RiSearchEyeLine  size={25} color="green" />
+        </button>
+      </div>
+
+      <main className="exibeDados">
+        <h2>Informações sobre a mercadoria:</h2>
+        <span>Local de origem: Rio de Janeiro/RJ</span>
+        <span>Local de destino: Belo Horizonte/MG</span>
+        <span>Situação: A caminho</span>
+      </main>
     </div>
   );
 }
