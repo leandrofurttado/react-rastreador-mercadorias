6d8c01eaf388357ea3bc52058e2c572eac45c071
Leandro Sousa <leandro.souza@southti.com.br>
2023-04-09 21:46:05 -0300

    Implementação da API do correios na Aplicação

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import {useState} from 'react';
 import { RiSearchEyeLine, RiShoppingCartFill } from "react-icons/ri";
+import MercadoriaDados from './MercadoriaDados.js';
 import './tracker.css';
 // o projeto é focado em realizar a busca por codigo de barras do correios
 
 function App() {
   const [input, setInput] = useState('');
+  const [dados, setDados] = useState([])
   console.log(input)
 
-  function handleSearch(){
-    alert("Mercadoria encontrada!")
+  async function handleSearch(){
+
+    try {
+      let procuraMercadoria = await fetch(`https://api.linketrack.com/track/json?user=teste&token=1abcd00b2731640e886fb41a8a9671ad1434c599dbaa0a0de9a5aa619f29a83f&codigo=${input}`);
+      const response = await procuraMercadoria.json()
+      const dadosMercadoria = Object.values(response);
+      const ocorrencias = dadosMercadoria[2].map((dado) => {
+        let data = dado.data;
+        let horario = dado.hora;
+        let local = dado.local;
+        let statusMercadoria = dado.status;
+        let subStatus = dado.subStatus;
+  
+        return { data, horario, local, statusMercadoria, subStatus };
+      });
+      setDados(ocorrencias)
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        alert("Você está pesquisando rapido demais, espere um momento e digite o código da mercadoria novamente!");
+      } else {
+        alert("Erro ao procurar mercadoria! Mercadoria não encontrada ou inválida!");
+      }
+    }
   };
 
 
@@ -32,10 +55,9 @@ function App() {
       </div>
 
       <main className="exibeDados">
-        <h2>Informações sobre a mercadoria:</h2>
-        <span>Local de origem: Rio de Janeiro/RJ</span>
-        <span>Local de destino: Belo Horizonte/MG</span>
-        <span>Situação: A caminho</span>
+        {dados.length > 0 ? ( // Renderiza MercadoriaDados apenas se ocorrencias não estiver vazio
+          <MercadoriaDados dados={dados} />
+        ):"Dados da mercadoria serão listados abaixo:"}
       </main>
     </div>
   );
---
+++ src/MercadoriaDados.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { RiArrowUpLine } from 'react-icons/ri'
+
+const MercadoriaDados = ({ dados }) => {
+  return (
+    <div>
+      {dados.map((ocorrencia, index) => (
+        <div key={index} style={{color: 'green'}}>
+          <p>Data: {ocorrencia.data}</p>
+          <p>Horário: {ocorrencia.horario}</p>
+          <p>Local: {ocorrencia.local}</p>
+          <p>Status Mercadoria: {ocorrencia.statusMercadoria}</p>
+          <p>{ocorrencia.subStatus}</p>
+          <br></br>
+          {index < dados.length - 1 && (
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: 10
+              }}
+            >
+              <RiArrowUpLine size={30} color="green" />
+            </div>
+          )}
+          <br></br>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MercadoriaDados;
